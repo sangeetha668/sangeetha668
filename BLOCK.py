7a8dcf6163a606a34e0369b3a9a27a9e69ba5102
sangeetha668 <reddysangeetha2005@gmail.com>
2025-08-18 18:44:05 +0530

    Update BLOCK.py

--- BLOCK.py
+++ BLOCK.py
@@ -1,17 +1,15 @@
-#block
+#BLOCK PATTERN
 start = int(input("Enter the stars (columns): "))
 block = int(input("Enter the number of blocks: "))
 lines = int(input("Enter the lines (rows): "))
-
-# Loop through blocks
 for i in range(block):
     count = 0
-    # Loop through rows
     for j in range(lines-i):
-        # Loop through columns
+        
         for k in range(start):
             print("*", end=" ")
             count += 1
         print()  
     print(count)   
     print("----------")
+
