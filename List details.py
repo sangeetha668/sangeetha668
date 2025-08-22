0861c4b134a3ac72feb13bff9b32936ce1b6b2ca
sangeetha668 <reddysangeetha2005@gmail.com>
2025-08-22 13:53:45 +0530

    Add files via upload

---
+++ List details.py
@@ -0,0 +1,47 @@
+"""a=["Mukesh","Mani","sree","pooz"]
+b=[[23,45,56,65],[89,76,45,6 7],[54,76,87,87],[55,33,53,27]]
+per=[]
+for i in b:
+    d=sum(i)//4
+    per.append(d)
+for i in range(4):
+    print("{}. {}: {}% ".format(i+1,a[i],per[i]))
+
+
+student=["anu","ani","bala","ravi","radha","srav"]
+
+cgpa=[59,66,47,88,8,69]
+arrear=[0,1,2,1,0,0]
+for i in range (6):
+    if arrear[i]==0 and cgpa[i]> 50:
+        print(student[i])
+
+import random
+l=[]
+for i in range(5):
+    d=random.randint(1,10)
+    if d not in l:
+        l.append(d)
+        
+
+for i in range(1,11):
+    for j in range(1,11):
+        print(i*j,end ="      ")
+        """
+a=20
+b=(a++ + ++)
+print(b)
+
+
+
+
+
+            
+
+            
+            
+
+
+    
+
+
