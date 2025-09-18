a649689816e00747d297837b819ceca2bde1f5a6
sangeetha668 <reddysangeetha2005@gmail.com>
2025-09-18 08:04:22 +0530

    Add files via upload

---
+++ COPRIME TRIPLE.py
@@ -0,0 +1,15 @@
+def gcd(a,b):
+    while b != 0:
+        rem = a % b
+        a = b
+        b = rem
+        return (a*b)
+def cop(a,b):
+    return gcd(a,b)
+
+a=int(input(" enter the number "))
+for i in range(5,a):
+    for j in range(4,i):
+        for k in range(3,j):
+            if(j*j+k*k==i*i and cop(i,j) and cop(j,k) and cop(i,k)):
+               print(k,j,i)
